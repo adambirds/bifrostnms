fcb848d58863c02de22e6758da427572be227c3f
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2026-08-15 03:47:14 +0100

    Add installation superuser support

---
+++ backend/bifrostnms/migrations/0002_rename_is_staff_to_is_superuser.py
@@ -0,0 +1,16 @@
+from tortoise import migrations
+from tortoise.migrations import operations as ops
+
+
+class Migration(migrations.Migration):
+    dependencies = [("models", "0001_initial")]
+
+    initial = False
+
+    operations = [
+        ops.RenameField(
+            model_name="User",
+            old_name="is_staff",
+            new_name="is_superuser",
+        ),
+    ]
